3c9280d79b64487bf0aab35a8a88de04cb1be6d8
KEDARUMA <kenichi@43z.tokyo>
2025-11-29 16:19:27 +0900

    fix

--- packages/revlm-client/src/index.ts
+++ packages/revlm-client/src/index.ts
@@ -4,3 +4,4 @@ export * from './Revlm.types';
 export { default as MdbCollection } from './MdbCollection';
 export { default as RevlmDBDatabase } from './RevlmDBDatabase';
 export { ObjectId, ObjectId as ObjectID } from 'bson';
+export * as BSON from 'bson';
